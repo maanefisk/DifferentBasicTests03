385265d47ae62788e7b582acbed8da2bbddd8b8f
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-08-29 10:53:29 +0200

    Sum of two integers, x3 if alike.

--- DifferentBasicTests03/Program.cs
+++ DifferentBasicTests03/Program.cs
@@ -7,6 +7,21 @@ namespace DifferentBasicTests03
         static void Main(string[] args)
         {
             //Test19 Compute the sum of two integers, if two values are equal :return the triple.
+            Console.Write("\nInsert Digit: ");
+            int digitOne = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nInsert Digit: ");
+            int digitTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nYour sum is: " + compareDigits(digitOne,digitTwo) + " ( x3 if alike )");
+        }
+
+        public static int compareDigits(int dOne, int dTwo)
+        {
+            int sum = dOne + dTwo;
+            if (dOne == dTwo) return sum + (sum / 2);
+            else
+            {
+                return sum;
+            }
         }
     }
 }
